804855330a6373f9bed911fef5156245d034248c
Thalyson Barros <61921408+ThalysonBarrosDev@users.noreply.github.com>
2021-07-19 16:38:23 -0300

    Add files via upload

--- MacGyver (React)/src/App.js
+++ MacGyver (React)/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
-import LogoHeader from './Imagens/logo--header.png';
+import logoHeader from './Imagens/logo--header.png';
 
 function App() {
   return (
     <div className="App">
 
       <header>
-        <div className="LogoHeader">
-          <img src={LogoHeader} />
+        <div className="logoHeader">
+          <img src={logoHeader} />
         </div>
       </header>
 
       <footer>
-        <div className="Rodape">
+        <div className="rodape">
+          <p>Copyright © <a href="https://www.superlagoa.com.br" target="_block">Super Lagoa</a> | Todos os direitos reservados.</p>
         </div>
       </footer>
 
